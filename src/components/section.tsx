feb01d5c078bfb70dcd711ba5a07a3b94f075add
Jorge Villalobos <jorge.villalobos2809@gmail.com>
2022-10-14 18:25:11 -0500

    feat: add content-visibility: auto to sections

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -12,7 +12,7 @@ export const Section = component$((props: SectionProps) => {
   return (
     <Wrapper
       class={[
-        "items-center",
+        "content-vibility-auto items-center",
         "tablet:first:pbs-8",
         variant !== "full-bleed" ? "tablet:pli-10 desktop:pli-41" : "",
         {
